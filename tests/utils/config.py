c4c37f4b30ba8ad03499213ed7d7ddab79949d33
Kat <65649991+00-kat@users.noreply.github.com>
2026-02-21 08:43:07 +1100

    Move config-related helpers to methods/properties of Config.

--- tests/utils/config.py
+++ tests/utils/config.py
@@ -1,9 +1,11 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, cast
 
 from app.config import Config, config_var
 
 if TYPE_CHECKING:
     from contextvars import Token
+
+    from app.bot import GhosttyBot
 
 
 def config() -> Token[Config]:
@@ -13,4 +15,4 @@ def config() -> Token[Config]:
         with config():
             ...
     """
-    return config_var.set(Config(".env.example"))
+    return config_var.set(Config(".env.example", bot=cast("GhosttyBot", object())))
